6baa7c732f69bdd83f9fd17c66848fb67c08c7b3
Lite Object/s <Lite.Object@gmail.com>
2022-08-23 19:36:37 +0000

    done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,48 @@
+import { useState } from 'react';
 import './App.css';
 
+// https://reactjs.org/docs/getting-started.html
+const Greeting = (props) => {
+  return (
+    <>
+      <h1>{props.text}</h1>
+      <h3>{props.language}</h3>
+    </>
+  )
+}
+
 function App() {
-  const name = 'World!!!';
-  const show = true;
+
+  const world = 'World!!!';
+  const [showInOtherLang, setShowInOtherLang] = useState(false);
+
+  function ShowOrHide(){
+    setShowInOtherLang(current => !current);
+  }
 
   return (
+
     <div className="App">
-      <h1>Hello {show? name: '...'}</h1>
+      <Greeting text={'Hello ' + world} language="English" />
+      {
+        showInOtherLang ?
+          (<>
+            <Greeting text="Hallo Welt" language="German" />
+            <Greeting text="Ciao mondo" language="Italian" />
+            <Greeting text="Hei maailma" language="Finnish" />
+            <Greeting text="Bonjour monde" language="French" />
+            <Greeting text="Selam Dünya" language="Turkish" />
+            <Greeting text="Sawubona Mhlaba" language="Zulu" />
+          </>) :
+          (
+            <>
+              <hr />
+            </>
+          )
+      }      
+      <button onClick={ShowOrHide} style={{color:'red', padding: '10px'}}>
+        {showInOtherLang ? 'Hide' : 'Show in' } other languages.
+      </button>
     </div>
   );
 }
